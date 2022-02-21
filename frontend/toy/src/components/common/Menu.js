bac1512d3dc836689f30be095467c49306ee6305
bamcoding <hanzen927@gmail.com>
2022-02-22 01:09:51 +0900

    update daily work

---
+++ frontend/toy/src/components/common/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                메뉴
+            </div>
+        );
+    }
+}
+
+export default Menu;
