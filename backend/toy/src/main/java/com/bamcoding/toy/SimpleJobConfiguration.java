7addfb3809e8fc1f98240ebe319f6ebe23db959f
bamcoding <hanzen927@gmail.com>
2022-03-08 16:18:03 +0900

    update to create a spring boot batch

---
+++ backend/toy/src/main/java/com/bamcoding/toy/SimpleJobConfiguration.java
@@ -0,0 +1,33 @@
+package com.bamcoding.toy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    Job simpleJob(){
+        return jobBuilderFactory.get("simpleJob").start(simpleStep1()).build();
+    }
+
+    @Bean
+    Step simpleStep1(){
+        return stepBuilderFactory.get("simpleStep1").tasklet((contribution, chunkContext) -> {
+            log.info("함수형 인터페이스 테스트릿이 작동중입니다.");
+            return RepeatStatus.FINISHED;
+        }).build();
+    }
+
+}
--- backend/toy/src/main/java/com/bamcoding/toy/ToyApplication.java
+++ backend/toy/src/main/java/com/bamcoding/toy/ToyApplication.java
@@ -1,10 +1,12 @@
 package com.bamcoding.toy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableBatchProcessing
 @SpringBootApplication
 public class ToyApplication {
 
