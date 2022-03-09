320cdeb85fe81d1220cc0a9919c4ca1a459861d1
bamcoding <hanzen927@gmail.com>
2022-03-09 15:37:09 +0900

    update 스프링 부트 배치 적용
    1. 배치 잡 메타 테이블 생성
    https://docs.spring.io/spring-batch/docs/current/reference/html/schema-appendix.html#metaDataBatchJobInstance
    
    2. 잡 config 설정
    
    3. 아래 방식으로 구현
    JobRepo > JobLauncher > Job > Step > Tasklet

--- backend/toy/src/main/java/com/bamcoding/toy/batch/SimpleJobConfig.java
+++ backend/toy/src/main/java/com/bamcoding/toy/batch/SimpleJobConfig.java
@@ -1,10 +1,11 @@
-package com.bamcoding.toy;
+package com.bamcoding.toy.batch;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class SimpleJobConfiguration {
+public class SimpleJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
@@ -23,6 +24,7 @@ public class SimpleJobConfiguration {
     }
 
     @Bean
+    @JobScope
     Step simpleStep1(){
         return stepBuilderFactory.get("simpleStep1").tasklet((contribution, chunkContext) -> {
             log.info("함수형 인터페이스 테스트릿이 작동중입니다.");
