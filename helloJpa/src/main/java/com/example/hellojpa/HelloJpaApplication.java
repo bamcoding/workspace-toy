5682116a171a9317453a05ccdc39082cae2f8faa
bamcoding <hanzen927@gmail.com>
2022-03-16 00:24:27 +0900

    update Swagger 2.9.2
    이슈:
    1. Spring boot 2.6.4 버전에서 Swagger 3.0.0 버전을 적용하려고 했는데 nullPointException이 발생한다.
    2. Spring boot 버전을 2.4.2로 했더니 에러가 해결되긴 했는데 운영중인 서비스에서 이런 방식으로 해결방법을 비효율적이고 영향도가 클것이라 예상된다... 다른 해결 방법은 없는 것일까?

--- helloJpa/src/main/java/com/example/hellojpa/HelloJpaApplication.java
+++ helloJpa/src/main/java/com/example/hellojpa/HelloJpaApplication.java
@@ -7,10 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class HelloJpaApplication {
-
     public static void main(String[] args) {
-        System.out.println("Hello JPA");
         SpringApplication.run(HelloJpaApplication.class, args);
     }
-
 }
