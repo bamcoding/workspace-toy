290bacdba87017fb9e16727d5655077454e761f3
Geun Jae Lee <hanzen927@gmail.com>
2022-09-15 23:37:54 +0900

    Update 9번 문제_키패드 누르기_220915.js

--- CodeTest/school.programmers.co.kr/9번 문제_키패드 누르기_220915.js
+++ CodeTest/school.programmers.co.kr/9번 문제_키패드 누르기_220915.js
@@ -10,25 +10,23 @@
 
 function solution(numbers, hand) {
     let tMap = {
-        '1' : [0,3], '2' : [1,3], '3' : [2,3],
-        '4' : [0,2], '5' : [1,2], '6' : [2,2],
-        '7' : [0,1], '8' : [1,1], '9' : [2,1],
-        '*' : [0,0], '0' : [1,0], '#' : [2,0],
+        1 : [0,3], 2 : [1,3], 3 : [2,3],
+        4 : [0,2], 5 : [1,2], 6 : [2,2],
+        7 : [0,1], 8 : [1,1], 9 : [2,1],
+        '*':[0,0], 0 : [1,0], '#':[2,0]
     }
 
     //0. 손위치 초기화
     let left = tMap['*'];
     let right = tMap['#'];
-
-    let result = '';
-
     
-    for(let i=0;i<numbers;i++){
+    let result = '';
+    for(let i=0;i<numbers.length;i++){
         
         //1. 입력한 숫자의 위치를 리턴
-        let cp = tMap[i+''];
-
-        if(cp[0] == 0 && cp[1] != 0) {
+        let cp = tMap[numbers[i]];
+        
+       if(cp[0] == 0 && cp[1] != 0) {
             
             //2. 특정 좌표는 무조건 L 리턴    
             result+='L';
@@ -45,15 +43,15 @@ function solution(numbers, hand) {
         
         //4. 입력된 숫자와 왼손엄지의 거리 계산
         lDiff = Math.abs(cp[0]-left[0]) + Math.abs(cp[1]-left[1]);
-        
+               
         //5. 입력된 숫자와 오른손 엄지의 거리 계산
         rDiff = Math.abs(cp[0]-right[0]) + Math.abs(cp[1]-right[1]);
-
+            
         //6. 오른손 거리와 왼손 거리를 비교해서 L, R을 리턴하고 손위치 저장
         if(lDiff<rDiff) {
             result+='L';
             left = cp;
-        } else if (lDiff<rDiff) {
+        } else if (lDiff>rDiff) {
             result+='R';
             right = cp;
         } else {
@@ -66,9 +64,7 @@ function solution(numbers, hand) {
             }
         }
     }
-    console.log(result);
     
-    var answer = result;
-    return answer;
+    return result;
 }
  
